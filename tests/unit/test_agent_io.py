26610b3d2fcd8ba68c298ad7ea8cca744801c963
Daniel M Campos <daniel.m.campos@icloud.com>
2021-08-16 11:17:08 -0500

    Move checkpoint to resources directory

--- tests/unit/test_agent_io.py
+++ tests/unit/test_agent_io.py
@@ -4,4 +4,4 @@ from deep_rl_navigation.agent import DQNAgent
 
 def test_load():
     agent = DQNAgent(state_size=8, action_size=4, seed=0)
-    agent_io.load(agent, "checkpoint.pth")
+    agent_io.load(agent, "../resources/checkpoint.pth")
