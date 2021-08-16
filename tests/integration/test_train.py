2ac2491d147281328930e3c5fead28f6d6941528
Daniel M Campos <daniel.m.campos@icloud.com>
2021-08-16 13:26:47 -0500

    Rename module to train

--- tests/integration/test_train.py
+++ tests/integration/test_train.py
@@ -2,14 +2,14 @@ import gym
 import pytest
 import torch
 
-from deep_rl_navigation import agent, training
+from deep_rl_navigation import agent, train
 
 
-def test_training():
+def test_train():
     agent.DEVICE = torch.device("cpu")
     dqn_agent = agent.DQNAgent(state_size=8, action_size=4, seed=0)
     env = gym.make("LunarLander-v2")
     env.seed(0)
-    scores = training.train(dqn_agent, env, n_episodes=1)
+    scores = train.train(dqn_agent, env, n_episodes=1)
     assert len(scores) == 1
     assert scores[0] == pytest.approx(-327.87563499560366)
