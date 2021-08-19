cba104032ccd3e6f4c4dfecb739006e8ac2f0680
Daniel M Campos <daniel.m.campos@icloud.com>
2021-08-19 13:17:05 -0500

    Refactor main
    - Use __main__ not main module
    - Use fire cli
    - Add play and train functions
    - Save default parameter trained agent with average score 14.89

--- src/deep_rl_navigation/__main__.py
+++ src/deep_rl_navigation/__main__.py
@@ -1,5 +1,69 @@
-from deep_rl_navigation.main import main
+import math
+
+import fire
+import torch
+
+from deep_rl_navigation import (
+    agent,
+    environment,
+    play as nav_play,
+    train as nav_train,
+    agent_io,
+)
 
 
-def __main__():
-    main()
+def play(
+    max_steps=math.inf,
+    load_path="data/checkpoint.pth",
+    device_type="cpu",
+    **agent_params,
+):
+    device = torch.device(device_type)
+    print(f"Training the Agent with {device.type} device")
+    agent.DEVICE = device
+    agent.DEVICE = torch.device("cpu")
+    navigation_env = environment.NavigationEnv.from_unity_binary(train_mode=False)
+    dqn_agent = agent.DQNAgent(
+        state_size=navigation_env.observation_space.shape[0],
+        action_size=navigation_env.action_space.n,
+        **agent_params,
+    )
+    agent_io.load(dqn_agent, load_path)
+    nav_play.play(dqn_agent, navigation_env, max_steps)
+
+
+def train(
+    n_episodes=2000,
+    max_t=1000,
+    eps_start=1.0,
+    eps_end=0.01,
+    eps_decay=0.995,
+    save_path="data/checkpoint.pth",
+    device_type="cpu",
+    **agent_params,
+):
+    device = torch.device(device_type)
+    print(f"Training the Agent with {device.type} device")
+    agent.DEVICE = device
+
+    navigation_env = environment.NavigationEnv.from_unity_binary()
+    dqn_agent = agent.DQNAgent(
+        state_size=navigation_env.observation_space.shape[0],
+        action_size=navigation_env.action_space.n,
+        **agent_params,
+    )
+    scores = nav_train.train(
+        dqn_agent,
+        navigation_env,
+        n_episodes=n_episodes,
+        max_t=max_t,
+        eps_start=eps_start,
+        eps_end=eps_end,
+        eps_decay=eps_decay,
+        filename=save_path,
+    )
+    # TODO: plot scores
+    print(scores)
+
+
+fire.Fire()
--- tests/integration/test_main.py
+++
@@ -1,5 +0,0 @@
-from deep_rl_navigation.main import main
-
-
-def test_main():
-    main(max_steps=25)
