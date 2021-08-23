12ac79d2f163dddb482bb39270ff9225c58a3cba
Daniel M Campos <daniel.m.campos@icloud.com>
2021-08-23 14:56:59 -0500

    Add goal line and first solved episode

--- src/deep_rl_navigation/plot.py
+++ src/deep_rl_navigation/plot.py
@@ -5,11 +5,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def performance(scores, figsize=(7, 7), save_file: Union[str, Path] = None):
+def performance(scores, figsize=(7, 7), save_file: Union[str, Path] = None, goal=13):
     fig, ax = plt.subplots(1, 1, figsize=figsize)
-    ax.plot(np.arange(len(scores)), scores)
+    scores = np.array(scores)
+    episodes = np.arange(scores.shape[0])
+    ax.plot(episodes, scores)
     ax.set_ylabel("Score")
     ax.set_xlabel("Episode #")
+
+    solved_episode = np.argmax(scores > goal)
+    ax.hlines(
+        goal,
+        episodes[1],
+        episodes[-2],
+        colors="red",
+        label=f"Goal {goal} achieved at episode #{solved_episode}",
+    )
+    ax.legend()
     if save_file:
         fig.savefig(save_file)
     else:
