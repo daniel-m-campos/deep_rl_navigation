1e79b6e79915c83dac49e83f73f94a7c537eb0b3
Daniel M Campos <daniel.m.campos@icloud.com>
2021-08-30 14:22:34 -0500

    Skip because it conflicts with other executing tests

--- tests/unit/test_import.py
+++ tests/unit/test_import.py
@@ -1,3 +1,4 @@
+import pytest
 from pkg_resources import parse_version
 from unityagents import UnityEnvironment
 
@@ -8,5 +9,6 @@ def test_version():
     assert parse_version(__version__) >= parse_version("1.0.0")
 
 
+@pytest.mark.skip
 def test_unity_environment():
     env = UnityEnvironment(file_name="/usr/local/sbin/Banana.x86_64")
